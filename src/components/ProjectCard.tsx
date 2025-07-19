e4450e75a5e2d502b859319e78973ead280f244e
Jakkmalm <152448498+Jakkmalm@users.noreply.github.com>
2025-07-19 22:02:07 +0200

    Update ProjectCard.tsx
    Fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Project } from '../app/data/projects';
 import AOS from 'aos';
-import 'aos/dist/aos.css
+import 'aos/dist/aos.css';
 
 interface Props {
     project: Project;
